8c543a0d23ebed1278fd0001d2d8681ef9f77060
marvintomizawa <marvintomizawa@hotmail.com>
2021-02-18 14:15:36 -0300

    feat: added scene transition

--- Assets/Scripts/SceneManagerBehaviour.cs
+++ Assets/Scripts/SceneManagerBehaviour.cs
@@ -1,16 +1,54 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneManagerBehaviour : MonoBehaviour
 {
+    [SerializeField] private Slider _slider;
+    [SerializeField] private bool _startWithTransition;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+        if (_startWithTransition)
+        {
+            _animator.SetTrigger("FadeOut");
+        }
+    }
+
     public void LoadTitleScreen()
     {
-        SceneManager.LoadScene("TitleScreen");
+        LoadSceneWithLoading("TitleScreen");
     }
 
     public void StartGame()
     {
-        SceneManager.LoadScene("Game");
+        LoadSceneWithLoading("Game");
+    }
+
+    private void LoadSceneWithLoading(string sceneName)
+    {
+        Time.timeScale = 1f;
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    private IEnumerator LoadSceneAsync(string sceneName)
+    {
+        _animator.SetTrigger("FadeIn");
+
+        yield return new WaitForSeconds(1f);
+
+        var operation = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            _slider.value = progress;
+            yield return null;
+        }
     }
 
     public void CloseGame()
