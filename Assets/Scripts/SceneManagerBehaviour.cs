68a3c4806150238d5f7cdeec42d2a37c4086f337
marvintomizawa <marvintomizawa@hotmail.com>
2021-02-18 11:46:03 -0300

    feat: added scene manager behaviour

---
+++ Assets/Scripts/SceneManagerBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerBehaviour : MonoBehaviour
+{
+    public void LoadTitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+}
